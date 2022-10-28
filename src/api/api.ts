58ab17b470c41de232c7de6ab6acf800e730354e
fdevos1 <felipe.devosm@gmail.com>
2022-10-28 15:11:49 -0300

    connecting to back-end

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3005",
+  baseURL: "https://crud-bot-canoas.herokuapp.com/",
   headers: {
     "Access-Control-Allow-Origin": "*",
   },
